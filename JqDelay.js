53a483ae3a9167d04c55087c0605d62d2d5b438e
jose marte <tumusicalatina@gmail.com>
2019-04-09 12:15:39 -0400

    Added using delay for animations

---
+++ JqDelay.js
@@ -0,0 +1,13 @@
+/**
+ * Chaining animation effects is a powerful tool in every jQuery developer's toolbox. 
+ * One of the more overlooked features is that you can introduce delays between animations.
+ */
+// This is wrong:
+$('#elem').animate({width:200},function(){
+    setTimeout(function(){
+        $('#elem').animate({marginTop:100});
+    },2000);
+});
+
+// Do it like this:
+$('#elem').animate({width:200}).delay(2000).animate({marginTop:100});
